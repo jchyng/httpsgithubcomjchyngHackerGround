ae2b7335a4e6fc77f10a7e576e0f1b9f6c8f52b2
jchyng <76997735+jchyng@users.noreply.github.com>
2023-06-22 14:07:40 +0900

    백엔드 코드 초기화

--- CrawlingNotiServer/src/main/java/com/hackerground/crawlingnotiserver/repository/job/JobCompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackerground.crawlingnotiserver.repository.job;
-
-import com.hackerground.crawlingnotiserver.entity.job.JobCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobCompanyRepository extends JpaRepository<JobCompany, String> {
-}
